7b6342323e67034077327bd59cb9110813a8c4cd
PedroAcevedo <pdacevedo@uninorte.edu.co>
2026-07-01 11:24:11 -0400

    refactor: CodeVR run

--- Unity/CodeVR/Assets/Settings/_Config.cs
+++
@@ -1,4 +0,0 @@
-public class _Config // TODO: Remove underscore here
-{
-    public static readonly string WebsiteIPAdress = "TODO"; // TODO: Write the IP adress where the blockly website is hosted on. It is usually your own computer
-}
